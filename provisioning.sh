c5ea7d512b4ef8b2aaf5231d10410e46cb751791
Mohammed Arif <m_arif1@yahoo.com>
2016-06-27 14:26:15 +0530

    Update provisioning.sh

--- provisioning.sh
+++ provisioning.sh
@@ -1,11 +1,6 @@
-export provisioning_home=
-
+#!/bin/bash
 sudo mkdir -p /apps/provisioning
-
 cd /apps/provisioning
-
-wget http://jenkins.wipdios.com:8082/nexus/content/repositories/delta/com/wipro/nt/provisioning/0.1.0/provisioning-0.1.0.jar
-
 export provisioning_home=/apps/provisioning
-
+wget http://jenkins.wipdios.com:8082/nexus/content/repositories/delta/com/wipro/nt/provisioning/0.1.0/provisioning-0.1.0.jar
 java -jar $provisioning_home/target/provisioning-0.0.1.jar
